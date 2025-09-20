17d6dce02a01b79047d8327f2e67ad234c618b32
Matheus <mfelipeleite8@gmail.com>
2025-09-20 17:41:34 -0300

    exercicio

---
+++ Desktop/atividade20-09/script.js
@@ -0,0 +1,63 @@
+//Exercico 1: Verificar maioridade
+
+const idade = Number(prompt("Digite sua idade:"));
+
+if (idade <= 17) {
+    console.log("menor de idade");
+} else if( idade >=18 ) {
+    console.log("maior de idade");
+}
+
+// //Exercico 2: Verificar se um número é positivo ou negativo
+
+const numero = Number(prompt("Digite um numero:"));
+
+if (numero <=0) {
+    console.log("negativo");
+} else if (numero >0) {
+    console.log("positivo");
+} else{
+    
+}
+
+//Exercico 3: Aprovação em uma prova
+
+const nota = Number(prompt("Digite a sua nota:"));
+
+if (nota <=59) {
+    console.log("REPROVADO")
+} else {
+    console.log("APROVADO")
+}
+
+//Exercicio 4: Verificar se um número é positivo, negativo ou zero
+
+const num = Number(prompt("Digite um numero:"));
+
+if (num <=0) {
+    console.log("negativo");
+} else if (num >0) {
+    console.log("positivo");
+} else{
+    console.log("zero");
+}
+
+//Exercico 5: Classificação de idade
+
+const age = Number(prompt("Digite sua idade:"))
+
+if (age <=12) {
+    console.log("Você é criança")
+} else if (age >=13) {
+    console.log("Você é adolescente")
+} else if (age >=18) {
+    console.log("Você é adulto")
+} else {
+    
+} {
+    
+} {
+    
+}
+
+
